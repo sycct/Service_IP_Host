d50768078e6612c269bf779598ae94c61a829ecd
sycct <huang_9119596@163.com>
2020-12-17 16:41:56 +0800

    2020-12-17 debug.

--- ip_host_main.py
+++ ip_host_main.py
@@ -74,7 +74,6 @@ class IPHostMain:
 if __name__ == '__main__':
     # 获取index配置参数
     get_config_file = os.path.join(os.getcwd(), 'files/index.txt')
-    print(get_config_file)
     if os.path.exists(get_config_file):
         with open(get_config_file, 'r') as csv_file:
             # 文件存在，读取1行index
