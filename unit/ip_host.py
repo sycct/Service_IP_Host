440ba055ac181e3af0bb8ecd6371827e6fbd4590
sycct <huang_9119596@163.com>
2021-04-17 09:34:54 +0800

    2021-4-17 missing ip host.

--- unit/ip_host.py
+++ unit/ip_host.py
@@ -7,6 +7,8 @@ import csv
 import os
 from concurrent import futures
 import IPy
+import requests
+from requests import exceptions
 
 from config import LoggingConfig
 
@@ -34,15 +36,15 @@ class IPHost:
 
     def missing_ip_range(self):
         # 中间缺失IP地址查询rDNS
-        start = IPy.IP('51.0.0.0').int()
-        end = IPy.IP('60.0.0.0').int()
+        start = IPy.IP('200.111.216.10').int()
+        end = IPy.IP('224.0.0.0').int()
         temp_list = []
         for int_ip in range(start, end, 1):
             # 将int ip装换成ip形式，加入到列表
             temp_list.append(IPy.intToIp(int_ip, version=4))
             # 队列100,000条数据开始循环，同时避免最后几条无法循环
-            if len(temp_list) > 100000 or int_ip > 1006538016:
-                self.query_many(temp_list)
+            if len(temp_list) > 100000 or int_ip > 3758073002:
+                self.query_many_by_google_dns(temp_list)
                 temp_list.clear()
 
     def other_ip_range(self):
@@ -76,10 +78,52 @@ class IPHost:
                 results.append(res)
         return len(results)
 
+    def query_many_by_google_dns(self, ip_list):
+        with futures.ThreadPoolExecutor(self._max_workers) as executor:
+            to_do = []
+            for item in ip_list:
+                future = executor.submit(self.get_host_by_google_dns, str(item))
+                to_do.append(future)
+
+                results = []
+                for future in futures.as_completed(to_do):
+                    res = future.result()
+                results.append(res)
+        return len(results)
+
     def get_host(self, ip):
         try:
             # 判断是bot，获取IP
             host = socket.gethostbyaddr(ip)[0]
+            host_ip = socket.gethostbyname(host)
+        except (socket.herror, socket.error, socket.gaierror, UnicodeError) as e:
+            # 验证失败，应该记录错误，以方便以后处理问题
+            self._init_log.error(f'通过IP解析Host的时候出现错误，错误内容：{e},IP地址：{ip}')
+            return False
+        if host_ip == ip:
+            # 如果验证通过，加入search_engine_bot_ip列表,之后显示正常页面
+            self.write_csv_file([ip, host])
+
+    def get_host_by_google_dns(self, ip):
+        ip = IP(ip)
+        reverse_name = ip.reverseName()
+        uri = f"https://dns.google/resolve?name={reverse_name}&type=PTR"
+        try:
+            get_result = requests.get(uri, timeout=5)
+        except (exceptions.ConnectionError, exceptions.HTTPError, exceptions.Timeout) as e:
+            self._init_log.error(f"网络出现错误，具体错误内容：{e}")
+            return False
+        result_json = get_result.json()
+        try:
+            get_answer = result_json['Answer'][0]
+        except KeyError:
+            self._init_log.error(f"没有找到Answer。")
+            return False
+        get_type = get_answer['type']
+        if get_type == 12:
+            # 转换成域名形式
+            host = get_answer['data'][0:-1]
+        try:
             host_ip = socket.gethostbyname(host)
         except (socket.herror, socket.error, socket.gaierror, UnicodeError) as e:
             # 验证失败，应该记录错误，以方便以后处理问题
